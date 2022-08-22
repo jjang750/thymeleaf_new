5f631e6d98f4e1f4539c9e43c83a81e85acf5cd5
방충일 <jjang750@aegisep.com>
2022-08-22 13:25:57 +0900

    add sign in

--- src/main/java/com/aegisep/thymeleaf/config/WebRestController.java
+++ src/main/java/com/aegisep/thymeleaf/config/WebRestController.java
@@ -1,7 +1,10 @@
 package com.aegisep.thymeleaf.config;
 
+import com.aegisep.thymeleaf.repository.CustomUserRepository;
+import com.aegisep.thymeleaf.user.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,9 @@ import java.util.UUID;
 @RestController
 public class WebRestController {
     private static final Logger log = LoggerFactory.getLogger(WebRestController.class);
+
+    @Autowired
+    CustomUserRepository customUserRepository;
 
     @RequestMapping(value="/pages")
     public ModelAndView paging(@RequestParam(value="gotoPage") String gotoPage) {
@@ -66,12 +72,35 @@ public class WebRestController {
 
         return view;
     }
+    @RequestMapping(value="/signin/checkuserid")
+    public String checkuserid(@RequestParam(value="userid") String userid) {
+        log.info("goto /signin/checkuserid");
+
+        User user =  customUserRepository.findByUserid(userid);
+
+        if (user == null) {
+            return "<li class='on' id='useridcheck'>" + userid + "을(를) 사용합니다.</li>";
+        }else{
+            return "<li id='useridcheck'>" + userid + "은(는) 이미 사용중입니다.</li>";
+        }
+    }
 
     @RequestMapping(value="/signing")
-    public ModelAndView signing() {
+    public ModelAndView signing(User userDTO) {
         log.info("goto signing");
         ModelAndView view = new ModelAndView();
         view.setViewName("/login");
+
+
+        log.info(">>>>>>>>>>>>>>>>>>>>> " + userDTO);
+
+        userDTO.setAuth_group("USER");
+        userDTO.setAuth_id("USER");
+
+        User insert = customUserRepository.save(userDTO);
+
+        log.info(">>>>>>>>>>>>>>>>>>>>> " + insert);
+
         return view;
     }
 
